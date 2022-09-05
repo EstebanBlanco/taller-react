61686a3f9ba55414ddaab5569767f325a3445071
EstebanBlanco <esteban.blanco@advisiondevelopment.com>
2022-09-04 23:29:27 -0600

    Componente base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import ProfileCard from './components/ProfileCard';
+
+const NAME = "Esteban Blanco"
+const AGE = 26
+const HOBBIES = ["Play video games", "Eat", "Travel", "Sleep"]
+
+const displayMessage = () => alert("Hola! :D")
+
+
 const App = () => {
   return (
     <div className="App">
-      My first app
+      <ProfileCard name={NAME} age={AGE} hobbies={HOBBIES} onClick={displayMessage} />
     </div>
   );
 }
